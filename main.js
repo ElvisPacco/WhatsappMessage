686f74e0b5294d041d596beb57852e4e40099524
ElvisPacco1 <elvispacco1@gmail.com>
2023-12-25 20:35:13 -0500

    Se agregó radio para elgir modo de envío y autofocus en celular

--- main.js
+++ main.js
@@ -1,33 +1,47 @@
-  function read(k){
-    cel = document.getElementById('celular');
-    cel.value = cel.value + k;
+function read(k){
+  cel = document.getElementById('celular');
+  cel.value = cel.value + k;
+}
+
+function eliminar() {
+  cel = document.getElementById('celular');
+  cel.value = "";
+}
+
+function borrar() {
+  cel = document.getElementById('celular');
+  cel.value = cel.value.substring(0,cel.value.length-1);
+}
+
+function CrearEnlace(){
+  cel = document.getElementById('celular').value;
+  men = document.getElementById('mensaje').value;
+  //men = "Hola.%0ATe saludo." //Salto de línea %0A, Espacio %20
+  modoEnvio = document.querySelectorAll('input[name=modo]');
+  mod = "web";
+  if(modoEnvio[1].checked){
+    mod = "api";
   }
-  
-  function eliminar() {
-    cel = document.getElementById('celular');
-    cel.value = "";
+
+  if(cel.length == 9){
+    window.open(`https://${mod}.whatsapp.com/send?phone=51${cel}&text=${men}`)
   }
-  
-  function borrar() {
-    cel = document.getElementById('celular');
-    cel.value = cel.value.substring(0,cel.value.length-1);
-  }
-  
-  function CrearEnlace(){
-    cel = document.getElementById('celular').value;
-    men = document.getElementById('mensaje').value;
-    //men = "Hola.%0ATe saludo." //Salto de línea %0A, Espacio %20
-    if(cel.length == 9){
-      window.open("https://wa.me/51" + cel + "?text=" + men)
+  else {
+    if (cel.length > 9) {
+      window.open(`https://${mod}.whatsapp.com/send?phone=${cel}&text=${men}`)
     }
     else {
-      if (cel.length > 9) {
-        window.open("https://wa.me/" + cel + "?text=" + men)
-      }
-      else {
-        if (confirm ("Posiblemente falte números. Para omitir este mensaje presione ACEPTAR.")) {
-          window.open("https://wa.me/51" + cel + "?text=" + men)
-        }
+      if (confirm ("Posiblemente falte números. Para omitir este mensaje presione ACEPTAR.")) {
+        window.open(`https://${mod}.whatsapp.com/send?phone=51${cel}&text=${men}`)
       }
     }
   }
+}
+
+document.addEventListener('keydown', PresionarEnter);
+function PresionarEnter(e){
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    CrearEnlace();
+  }
+}
--- main3.js
+++ main3.js
@@ -120,17 +120,22 @@ function CrearEnlace(){
   //men = document.getElementById('mensaje').value.split('\n');
   men = document.getElementById('mensaje').value.replaceAll("\n","%0A");
   //men = "Hola.%0ATe saludo." //Salto de línea %0A, Espacio %20
+  modoEnvio = document.querySelectorAll('input[name=modo]');
+  mod = "web";
+  if(modoEnvio[1].checked){
+    mod = "api";
+  }
 
   if(cel.length == 9){
-    window.open("https://wa.me/51" + cel + "?text=" + men)
+    window.open(`https://${mod}.whatsapp.com/send?phone=51${cel}&text=${men}`)
   }
   else {
     if (cel.length > 9) {
-      window.open("https://wa.me/" + cel + "?text=" + men)
+      window.open(`https://${mod}.whatsapp.com/send?phone=${cel}&text=${men}`)
     }
     else {
       if (confirm ("Posiblemente falte números. Para omitir este mensaje presione ACEPTAR.")) {
-        window.open("https://wa.me/51" + cel + "?text=" + men)
+        window.open(`https://${mod}.whatsapp.com/send?phone=51${cel}&text=${men}`)
       }
     }
   }
@@ -161,25 +166,33 @@ function CrearConcatenar(){
   }
 
   function CrearConcatenarApi(){
-    cel = document.getElementById('celular').value;
-    //men = document.getElementById('mensaje').value.split('\n');
-    men = document.getElementById('mensaje').value.replaceAll("\n","%0A");
-    //men = "Hola.%0ATe saludo." //Salto de línea %0A, Espacio %20
-  
-    if(cel.length == 9){
-      concat = "https://api.whatsapp.com/send?phone=51[CELDA_CELULAR]&text=" + men
+  cel = document.getElementById('celular').value;
+  //men = document.getElementById('mensaje').value.split('\n');
+  men = document.getElementById('mensaje').value.replaceAll("\n","%0A");
+  //men = "Hola.%0ATe saludo." //Salto de línea %0A, Espacio %20
+
+  if(cel.length == 9){
+    concat = "https://api.whatsapp.com/send?phone=51[CELDA_CELULAR]&text=" + men
+  }
+  else {
+    if (cel.length > 9) {
+      concat = "https://api.whatsapp.com/send?phone=[CELDA_CELULAR]&text=" + men
     }
     else {
-      if (cel.length > 9) {
-        concat = "https://api.whatsapp.com/send?phone=[CELDA_CELULAR]&text=" + men
-      }
-      else {
-        if (confirm ("Posiblemente falte números. Para omitir este mensaje presione ACEPTAR.")) {
-            concat = "https://api.whatsapp.com/send?phone=51[CELDA_CELULAR]&text=" + men
-        }
+      if (confirm ("Posiblemente falte números. Para omitir este mensaje presione ACEPTAR.")) {
+          concat = "https://api.whatsapp.com/send?phone=51[CELDA_CELULAR]&text=" + men
       }
     }
-    concat = '=HIPERVINCULO(CONCATENAR("' + concat + '"))';
-    concat = concat.replaceAll('[','";').replaceAll(']',';"');
-    document.getElementById('concatenar').value = concat;   
   }
+  concat = '=HIPERVINCULO(CONCATENAR("' + concat + '"))';
+  concat = concat.replaceAll('[','";').replaceAll(']',';"');
+  document.getElementById('concatenar').value = concat;   
+}
+
+document.addEventListener('keydown', PresionarEnter);
+function PresionarEnter(e){
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    CrearEnlace();
+  }
+}
